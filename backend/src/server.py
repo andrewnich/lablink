6049ca172837d65487b8284f230ca36e9979bdb6
Andrew Nicholson <andrewnicholsonau@gmail.com>
2021-07-18 02:22:39 +1000

    FIXING MY STUPID BRAIN

--- backend/src/server.py
+++ backend/src/server.py
@@ -58,7 +58,7 @@ def server_login():
     '''Login request handler'''
     data = request.get_json()
     uid = auth.login(data['email'], data['password'])
-    if uid != -1:
+    if uid != "-1":
         response = {
             "uid": uid,
             "status": 200
@@ -78,7 +78,7 @@ def server_login():
 def server_register():
     data = request.get_json()
     uid = auth.register(data['email'], data['password'])
-    if uid != -1:
+    if uid != "-1":
         response = {
             "uid": uid,
             "status": 200
