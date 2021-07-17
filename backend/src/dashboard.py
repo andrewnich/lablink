52248e00eace326d8b7071833ef99629bd12e656
Tchhoeu <tomchhoeuemail@gmail.com>
2021-07-17 20:53:28 +1000

    dashboard stuff:

--- backend/src/dashboard.py
+++ backend/src/dashboard.py
@@ -1,7 +1,35 @@
-def generatedashboard(uid:str)->list:
+import json
 
+fp = "../data/labs.json"
+
+"""
+Generates first page of dashboard
+"""
+def generated_dashboard(uid:str)->list:
+    #TODO Limit the length of the list
+    
+    with open(fp) as labsJson:
+        labs = json.load(labsJson)
+        labsJson.close()
+    lab_list = []
+    for lab in labs["labs"]:
+        lab_list.append({"Name": lab["Name"], "Video": lab["Start"]["Video"]})
     return lab_list
 
+"""
+Generates first page of search results
+"""
 def search(search_term:str)->list:
-
+    #TODO Work out how to do a proper search algorithm
+    with open(fp) as labsJson:
+        labs = json.load(labsJson)
+        labsJson.close()
+    lab_list = []
+    for lab in labs["labs"]:
+        if search_term in lab["Name"]:
+            lab_list.append({"Name": lab["Name"], "Video": lab["Start"]["Video"]})
+    
     return lab_list
+
+# print(generated_dashboard("lmao"))
+# print(search("no"))
