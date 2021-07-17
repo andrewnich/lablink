aae734fdb4584c6e7b658127ea53cbb34c87648f
julian <julian.batongbacal@gmail.com>
2021-07-17 19:05:37 +1000

    login formatting

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,20 +4,17 @@ import { makeStyles } from '@material-ui/core/styles';
 import { FormHelperText } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 
-import { useHistory, Link } from 'react-router-dom'
+import { useHistory, Link } from 'react-router-dom';
 
-const useStyles = makeStyles (() => ({
-  logo: {
-
-  },
+const useStyles = makeStyles(() => ({
+  logo: {},
 
   signIn: {
     display: 'block',
     fontSize: '30px',
     padding: '0px',
-    margin: '20px'
-
-  }, 
+    margin: '20px',
+  },
 
   signInContainer: {
     display: 'flex',
@@ -27,7 +24,7 @@ const useStyles = makeStyles (() => ({
     boxShadow: '0px 0px 10px 2px #3d3d3d',
     padding: 'px',
     borderRadius: '5px',
-    alignItems: 'center'
+    alignItems: 'center',
   },
 
   signInBody: {
@@ -35,50 +32,82 @@ const useStyles = makeStyles (() => ({
   },
 
   signInLabel: {
-    textAlign: 'left'
+    textAlign: 'left',
   },
 
-  textEntry : {
-    margin: '10px'
+  textEntry: {
+    margin: '10px',
   },
 
-  forgotPassword : {
+  forgotPassword: {
     textAlign: 'right',
     fontSize: '10px',
-    display: 'block'
+    display: 'block',
   },
 
-  horizontalFlexContainer : {
-    display: 'flex'
+  horizontalFlexContainer: {
+    display: 'flex',
   },
 
-  button : {
+  button: {
     width: '50%',
     margin: '5px',
-  }
+  },
 }));
 
 const Login = () => {
   const classes = useStyles();
   const history = useHistory();
 
-  
   return (
-  <div>
+    <div>
       <p className={classes.logo}>LabLink</p>
-      <div className={classes.signInContainer}>
-        <p className={classes.signIn}>Sign In</p>
-        <form>
-          <TextField className={classes.textEntry} variant='outlined' label='Email address' name="email" required/>
-          <TextField className={classes.textEntry} variant='outlined' label='Password' type='password' name="password" required/>
-          <a classNames={classes.forgotPassword} href="">Forgot your password?</a>
-          <Button className={classes.button} color="primary" variant="contained" type="submit" fullWidth>Log In</Button>
-        </form>
-        <Button className={classes.button} color="secondary" variant="contained" fullWidth component={Link} to='/signup'>Sign Up</Button>
-      </div>
-  </div>
+      <form>
+        <div className={classes.signInContainer}>
+          <p className={classes.signIn}>Sign In</p>
+
+          <TextField
+            className={classes.textEntry}
+            variant='outlined'
+            label='Email address'
+            name='email'
+            required
+          />
+          <TextField
+            className={classes.textEntry}
+            variant='outlined'
+            label='Password'
+            type='password'
+            name='password'
+            required
+          />
+          <a classNames={classes.forgotPassword} href=''>
+            Forgot your password?
+          </a>
+          <Button
+            className={classes.button}
+            color='primary'
+            variant='contained'
+            type='submit'
+            fullWidth
+          >
+            Log In
+          </Button>
+
+          <Button
+            className={classes.button}
+            color='secondary'
+            variant='contained'
+            fullWidth
+            component={Link}
+            to='/signup'
+          >
+            Sign Up
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 };
- 
 
 export default Login;
