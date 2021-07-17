6fff09cabc076538d8122ccd69d2fb5cf0d4acf4
Tchhoeu <tomchhoeuemail@gmail.com>
2021-07-17 22:59:12 +1000

    checkpoint

--- backend/src/lab.py
+++ backend/src/lab.py
@@ -1,6 +1,15 @@
-def selectlab(labid:str)->str:
+import json
 
-    return labjson
+fp = "../data/labs.json"
+
+def select_lab(labid:int)->str:
+    with open(fp) as labsJson:
+        labs = json.load(labsJson)
+        labsJson.close()
+    for lab in labs["labs"]:
+        if labid == lab["Id"]:
+            return lab
+    return None
 
 def next_step(answer:str)->str:
 
@@ -9,3 +18,5 @@ def next_step(answer:str)->str:
 def previous_step()->str:
 
     return labnode
+
+print(select_lab(1))
