3764d5f825b1103515628650e4a6ccbbb550ec6c
225544 <murraypeh@gmail.com>
2021-07-17 20:18:42 +1000

    Register

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -14,9 +14,7 @@ def login(email:str, password:str)->str:
 
     d = os.getcwd()
     d = os.path.join(d,'/src/data.json')
-
     f = open(d,)
-
     data = json.load(f)
     
     hash_to_check = hash_p(password)
@@ -25,9 +23,10 @@ def login(email:str, password:str)->str:
     for i in range(0,len(data)):
         if data[i][0] == email:
             if data[i][1] == hash_to_check:
-                uid = data[i][2]
+                uid = i
                 break
     
+    f.close()
     return uid  
 
 
@@ -37,6 +36,33 @@ def logout(uid:str) ->bool:
 
 
 def register(email:str, password:str)->str:
+    d = os.getcwd()
+    d = os.path.join(d,'/src/data.json')
+    f = open(d,)
+    data = json.load(f)
+    f.close()
+
+    email_exists = False
+
+    for i in range(0,len(data)):
+        if data[i][0] == email:
+            #Email already in Database
+            email_exists = True
+            uid = i
+            break
+    
+    if email_exists == False:
+        uid = len(data)+1
+        data[uid] = []
+        data[uid][0]=email
+        data[uid][1]=hash_p(password)
+
+        uid = len(data)
+
+        f = open(d,'w')
+        f.write(data)
+        f.close()
+
 
     return uid
 
