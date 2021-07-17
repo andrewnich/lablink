806dba775776da668c62f1a935c749643248a0a0
julian <julian.batongbacal@gmail.com>
2021-07-17 23:57:13 +1000

    using react yt library now and implemented dialog pop up on video end

--- frontend/src/components/YoutubeEmbed.js
+++ frontend/src/components/YoutubeEmbed.js
@@ -1,19 +1,12 @@
 import React from 'react';
+import YouTube from 'react-youtube';
 
 const YoutubeEmbed = (props) => {
-  return (
-    <div>
-      <iframe
-        width='853'
-        height='480'
-        src={`https://www.youtube.com/embed/${props.youtubeId}`}
-        frameBorder='0'
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-        allowFullScreen
-        title='Embedded youtube'
-      />
-    </div>
-  );
+  const handleVideoEnd = () => {
+    console.log('video ended!');
+  };
+
+  return <YouTube videoId={props.youtubeId} onEnd={handleVideoEnd} />;
 };
 
 export default YoutubeEmbed;
--- frontend/src/pages/LabDemo.js
+++ frontend/src/pages/LabDemo.js
@@ -1,15 +1,48 @@
 import React from 'react';
 
-import Container from '@material-ui/core/Container';
+import { Container, Grid, Button } from '@material-ui/core';
 import { useParams } from 'react-router';
-import YoutubeEmbed from '../components/YoutubeEmbed';
+import YouTube from 'react-youtube';
+
+import LabChoiceDialog from '../components/LabChoiceDialog';
 
 const LabDemo = () => {
   const { labID } = useParams();
+
+  // state for lab choice dialog
+  const [labChoiceOpen, setLabChoiceOpen] = React.useState(false);
+
+  const handleVideoEnd = () => {
+    console.log('video ended!');
+    setLabChoiceOpen(true);
+  };
+
+  const handleLabChoiceClose = () => {
+    setLabChoiceOpen(false);
+  };
+
+  const labMainStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  };
+
   return (
-    <Container component='main' maxWidth='md' className='main'>
+    <Container
+      component='main'
+      maxWidth='md'
+      className='main'
+      style={labMainStyle}
+    >
       <h1>This is lab demo with id {labID}</h1>
-      <YoutubeEmbed youtubeId='dQw4w9WgXcQ' />
+      <LabChoiceDialog open={labChoiceOpen} onClose={handleLabChoiceClose} />
+      <YouTube videoId={'dQw4w9WgXcQ'} onEnd={handleVideoEnd} />
+      <Grid container justify='center' direction='row'>
+        <Button variant='contained' color='secondary'>
+          Go back a step
+        </Button>
+        <Button variant='contained'>Restart lab</Button>
+      </Grid>
     </Container>
   );
 };
