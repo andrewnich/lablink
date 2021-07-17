5a0860911f55c7e28c82b16ce0e77ccee7e29a3f
225544 <murraypeh@gmail.com>
2021-07-17 20:01:46 +1000

    Login and Hash Function V1

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -1,6 +1,34 @@
+import json
+import os
+import os.path
+import shutil
+import hashlib, uuid
+
+def hash_p(password:str)->str:
+    salt = uuid.uuid4().hex
+    hashed_password = hashlib.sha512(password + salt).hexdigest()
+    return hashed_password
+
+
 def login(email:str, password:str)->str:
 
-    return uid
+    d = os.getcwd()
+    d = os.path.join(d,'/src/data.json')
+
+    f = open(d,)
+
+    data = json.load(f)
+    
+    hash_to_check = hash_p(password)
+
+    uid = "0"
+    for i in range(0,len(data)):
+        if data[i][0] == email:
+            if data[i][1] == hash_to_check:
+                uid = data[i][2]
+                break
+    
+    return uid  
 
 
 def logout(uid:str) ->bool:
